47249912adb52b40891a1ad96cf31c772e8247d5
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-03-05 00:02:37 +0800

    third commit

--- auto_selfie.py
+++ auto_selfie.py
@@ -1,12 +1,15 @@
 import cv2
 
 video_capture = cv2.VideoCapture(0)
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-smileCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_smile.xml")
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_smile.xml")
+
 while True:
-    ret, image = video.read()
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray, 1.1, 5)
+    ret, image = video_capture.read()
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    
     for(x,y,w,h) in faces:
         cv2.rectangle(image, (x,y), (x+w, y+h),(0,0,0),3)
         smileCascade.detectMultiScale(gray, 1.8, 15)
